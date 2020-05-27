e81b77673bef01094d183a43fed40139602052c4
Leslie <leshermira@gmail.com>
2020-05-27 16:31:24 -0400

    v.2

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const numberCard = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+const numberCard = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 const cardSuit = ['&spades;', '&clubs;', '&hearts;', '&diams;'];
 
 function randomSelection() {
 
-    let randomNumber = numberCard[Math.floor(Math.random()*numberCard.length)]; // para generar numero aleatorio
-    let randomCardsuit = cardSuit[Math.floor(Math.random()*cardSuit.length)]; // para generar pinta aleatoria
+    //Genera numero y pinta aleatoria
+    let randomNumber = numberCard[Math.floor(Math.random()*numberCard.length)]; 
+    let randomCardsuit = cardSuit[Math.floor(Math.random()*cardSuit.length)]; 
 
+    //imprime datos en HTML 
     document.getElementById("topCard").innerHTML = randomCardsuit; 
     document.getElementById("bottomCard").innerHTML = randomCardsuit;
-    document.getElementById("number").innerHTML = randomNumber; // para imprimir numero aleatorio
+    document.getElementById("number").innerHTML = randomNumber; 
 
     // generar condicion para que cuando salga corazon o diamante, cambie la pinta a color rojo
-
     if(randomCardsuit === "&hearts;" || randomCardsuit === "&diams;"){
         topCard.style.color = 'red';
         bottomCard.style.color = 'red';
